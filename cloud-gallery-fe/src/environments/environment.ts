4155bcf96493bf23ce9ae49fbe7f178ae7260a1b
mszrsblnt <94174869+mszrsblnt@users.noreply.github.com>
2026-03-28 13:39:45 +0100

    Add dockerfile

--- cloud-gallery-fe/src/environments/environment.ts
+++ cloud-gallery-fe/src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://backend-4nln.onrender.com',
-  appTitle: 'Cloud Gallery',
+  apiUrl: '__API_URL__',
+  appTitle: 'Cloud Gallery'
 };
